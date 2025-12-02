c874653ac5d65eb205602a608d7aa3d443e7950c
tzha0142 <Zty000429@outlook.com>
2025-12-03 05:46:18 +0800

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/slection/',
+  base: '/Frontend_test/',
   server: {
     proxy: {
       '/api': {
